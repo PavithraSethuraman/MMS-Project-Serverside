d40a54499700a0ff23583a4aa513cb43d0a9f1c1
Pavithra Sethuraman <pavithrasethuraman92@gmail.com>
2022-09-07 15:19:33 +0530

    commited

---
+++ models/Dcm.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const DcmSchema = new mongoose.Schema(
+  {
+    dcmId: { type: String },
+    userId: { type: String },
+    employeeName: { type: String },
+    customerName: { type: String, required: true },
+    contactPerson: { type: String, required: true },
+    email: { type: String, required: true },
+    mobile: { type: String, required: true },
+    inTime: { type: String, required: true },
+    outTime: { type: String, required: true },
+    needs: { type: String, required: true },
+    expectedDate: { type: String, required: true },
+    remarks: { type: String, required: true },
+    status: { type: String },
+    createDate: { type:String},
+    createTime: { type:String},
+    modifyBy: { type: String, required: false },
+    modifyDate: { type: String, required: false },
+    action:{type:String}
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Dcm", DcmSchema);
---
+++ routes/dcm.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  createDcm,
+  deleteDcm,
+  getDcm,
+  getDcms,
+  updateDcm,
+  getDcmDetailsSingleUser
+} from "../controllers/dcm.js";
+
+const router = express.Router();
+//CREATE
+router.post("/:userId", createDcm);
+
+//UPDATE
+router.put("/:id", updateDcm);
+//DELETE
+router.delete("/:id", deleteDcm);
+//GET
+
+router.get("/:id", getDcm);
+//GET ALL
+
+router.get("/", getDcms);
+
+router.get("/dcmUser/:id", getDcmDetailsSingleUser);
+
+export default router;
